824127a22bb218005c7fb731d801e41d5a90aa94
SEONGBEOM-KIM <59245627+SEONGBEOM-KIM@users.noreply.github.com>
2020-02-25 16:42:52 +0900

    add link, save to csv file

--- so.py
+++ so.py
@@ -18,13 +18,14 @@ def extract_job(html):
     company, location = html.find("h3").find_all("span", recursive=False)
     company = company.get_text(strip=True)
     location = location.get_text(strip=True)
-    print(company, location)
-    return {"title": title, "company": company, "locaion": location}
+    job_id = html["data-jobid"]
+    return {"title": title, "company": company, "locaion": location, "link": f"https://stackoverflow.com/jobs/{job_id}/"}
 
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping SO: page {page}")
         result = requests.get(f"{URL}&pg={page + 1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
